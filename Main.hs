cebca402bda3b2117eaa35d3da0101302b1edcfe
Dave Compton <davecompton7@gmail.com>
2016-12-06 03:26:24 +0000

    switched to using forM instead of listHoldWithKey

--- Main.hs
+++ Main.hs
@@ -5,6 +5,7 @@ import Reflex.Dom
 import Control.Monad.Trans (liftIO)
 import Data.Map as DM (Map, fromList)
 import Data.Text (Text, pack)
+import Data.Traversable (forM)
 import Data.Time.Clock (getCurrentTime)
 
 type Pos = (Int, Int)
@@ -18,9 +19,6 @@ h = 80
 cellSize :: Int
 cellSize = 20
 
-mkBoard :: Map Pos ()
-mkBoard = 
-    fromList $ zip [(x,y) | x <- [0..w-1], y <- [0..h-1]] $ repeat ()
 
 cellAttrs :: Map Text Text
 cellAttrs = 
@@ -38,8 +36,8 @@ groupAttrs (x,y) =
                )
              ] 
 
-showCell :: MonadWidget t m => Pos -> () -> m ()
-showCell pos () = do
+showCell :: MonadWidget t m => Pos -> m ()
+showCell pos = do
     elSvgns "g"  (constDyn $ groupAttrs pos) $ 
         elSvgns "rect" (constDyn $ cellAttrs) $ 
             return ()
@@ -49,19 +47,15 @@ boardAttrs :: Map Text Text
 boardAttrs = fromList 
                  [ ("width" , pack $ show $ w * cellSize)
                  , ("height", pack $ show $ h * cellSize)
-                 , ("style" , "border:dotted; margin:8em")
+                 , ("style" , "border:solid; margin:8em")
                  ]
 
 main :: IO ()
 main = mainWidget $ do 
-           now <- liftIO getCurrentTime 
-           ticker <- fmap mempty <$> tickLossy 1.0 now
-           rec 
-               let 
-                   ev = listHoldWithKey mkBoard ticker showCell
-               elSvgns "svg" (constDyn boardAttrs) ev
-               cm <- ev  -- slows load time down quite a bit.
+           let indices = [(x,y) | x <- [0..w-1], y <- [0..h-1]] 
+           elSvgns "svg" (constDyn boardAttrs) $ forM indices showCell
            return ()
+
 
 elSvgns :: MonadWidget t m => Text -> Dynamic t (Map Text Text) -> m a -> m (El t, a)
 elSvgns = elDynAttrNS' (Just "http://www.w3.org/2000/svg")
