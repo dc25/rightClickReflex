5e28d9bea37c54a07dbda859b1ee3453060a984a
Dave Compton <davecompton7@gmail.com>
2016-12-20 13:42:08 -0800

    trimmed down

--- Main.hs
+++ Main.hs
@@ -2,33 +2,25 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Reflex
 import Reflex.Dom
-import Data.Map as DM (Map, fromList)
+import Data.Map as DM (Map)
+import Data.Monoid ((<>))
 import Data.Text (Text, pack)
 
 elSvgns :: MonadWidget t m => Text -> Dynamic t (Map Text Text) -> m a -> m (El t, a)
 elSvgns = elDynAttrNS' (Just "http://www.w3.org/2000/svg")
 
+size = "300"
+
 rectAttrs :: Bool -> Map Text Text
-rectAttrs c = 
-    fromList [ ( "width",        "100%")
-             , ( "height",       "100%")
-             , ( "style",        pack $ "fill:" ++ if c then "red" else "black")
-             , ( "oncontextmenu", "return false;")
-             ] 
+rectAttrs c =   "width" =:        size
+             <> "height" =:       size
+             <> "style" =:        (pack $ "fill:" ++ if c then "red" else "black")
+             <> "oncontextmenu" =: "return false;"
 
-svgAttrs :: Map Text Text
-svgAttrs = fromList 
-                 [ ("width" , "100")
-                 , ("height", "100")
-                 ]
-
-update _  = not 
-
-main :: IO ()
 main = mainWidget $ do 
-                rec 
-                    rb <- foldDyn update False ev
-                    (_, ev) <- elSvgns "svg" (constDyn svgAttrs) $ do
-                        (el, _) <- elSvgns "rect" (fmap rectAttrs rb) $ return ()
-                        return $ () <$ leftmost [domEvent Click el , domEvent Contextmenu el]
-                return ()
+           rec 
+               rb <- foldDyn (\e b -> not b) False ev
+               (_, ev) <- elSvgns "svg" (constDyn $ "width" =: size <> "height" =: size) $ do
+                   (el, _) <- elSvgns "rect" (fmap rectAttrs rb) $ return ()
+                   return $ leftmost [domEvent Click el , domEvent Contextmenu el]
+           return ()
